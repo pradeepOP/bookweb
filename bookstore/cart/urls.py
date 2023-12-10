5570449f2d1ed9e8292e629dcc66c63e1bf376a0
pradeepOP <75737166+pradeepOP@users.noreply.github.com>
2023-12-10 18:56:42 +0545

    some small changes

--- bookstore/cart/urls.py
+++ bookstore/cart/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path(
         "update_cart/<int:book_id>/<str:action>/", views.update_cart, name="update_cart"
     ),
-    path("checkout", views.checkout, name="checkout"),
+    path("checkout/", views.checkout, name="checkout"),
     path("hx_menu_cart/", views.hx_menu_cart, name="hx_menu_cart"),
     path("hx_cart_total/", views.hx_cart_total, name="hx_cart_total"),
 ]
